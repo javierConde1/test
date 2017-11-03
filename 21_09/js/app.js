58bcd3863177e44ddcb8131d07f6f0f2c7ed936f
javierConde1 <javier_conde101@yahoo.com.mx>
2017-11-03 14:36:57 -0700

    03-11

--- 21_09/js/app.js
+++ 21_09/js/app.js
@@ -9,3 +9,11 @@ var rectac = new Rect(100,150,150,250)
 var circulo = new Circle(500,250,100,0,150)
 .addTo(stage)
 .attr('fillColor','cyan');
+
+var estrella = new Star(50,500,10,10,5)
+.addTo(stage)
+.attr('fillColor','cyan');
+
+var poligono = new Ellipse(500,500,150,100)
+.addTo(stage)
+.attr('fillColor','brown');
